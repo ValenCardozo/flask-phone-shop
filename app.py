e2af247846fad8477defa3aeb2efc535a54ec8a3
germanalvarez8 <ger.22.alvarez@gmail.com>
2024-07-29 01:06:20 -0300

    Migrations, models, and first endpoints

--- app.py
+++ app.py
@@ -3,13 +3,25 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:8889/vehicles'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/flask_phone_shop'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from models import Brand, Type, Vehicle
+from models import Marca, Fabricante, Modelo, Categoria, Caracteristica, Equipo, Stock, Proveedor, Accesorio
 
 @app.route("/")
-def home(): 
+def home():
     return render_template("index.html")
+
+@app.route("/marcas", methods=['POST', 'GET'])
+def marcas():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        nueva_marca = Marca(nombre=nombre)
+        db.session.add(nueva_marca)
+        db.session.commit()
+
+    marcas = Marca.query.all()
+    return render_template("marcas.html", marcas=marcas)
---
+++ models.py
@@ -0,0 +1,53 @@
+from app import db
+
+class Marca(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+class Fabricante(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    pais_origen = db.Column(db.String(50), nullable=False)
+
+class Modelo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    fabricante_id = db.Column(db.Integer, db.ForeignKey('fabricante.id'), nullable=False)
+    fabricante = db.relationship('Fabricante', backref=db.backref('modelos', lazy=True))
+
+class Categoria(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+
+class Caracteristica(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tipo = db.Column(db.String(50), nullable=False)
+    descripcion = db.Column(db.String(200))
+
+class Equipo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    modelo_id = db.Column(db.Integer, db.ForeignKey('modelo.id'), nullable=False)
+    categoria_id = db.Column(db.Integer, db.ForeignKey('categoria.id'), nullable=False)
+    costo = db.Column(db.Float, nullable=False)
+    modelo = db.relationship('Modelo', backref=db.backref('equipos', lazy=True))
+    categoria = db.relationship('Categoria', backref=db.backref('equipos', lazy=True))
+
+class Stock(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    equipo_id = db.Column(db.Integer, db.ForeignKey('equipo.id'), nullable=False)
+    cantidad_disponible = db.Column(db.Integer, nullable=False)
+    ubicacion = db.Column(db.String(100), nullable=False)
+    equipo = db.relationship('Equipo', backref=db.backref('stock', lazy=True))
+
+class Proveedor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    contacto = db.Column(db.String(100), nullable=False)
+
+class Accesorio(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tipo = db.Column(db.String(50), nullable=False)
