29871da1636f17f9e461d48615c44c567cff9d02
ValenCardozo <valencardozo7@gmail.com>
2024-08-10 12:53:32 -0300

    feat(crud): add delete and edit

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def marcas():
     marcas = Marca.query.all()
     return render_template("marcas.html", marcas=marcas)
 
-@app.route("/fabricantes", methods=['POST', 'GET'])
+@app.route("/fabricantes", methods=['POST', 'GET', 'DELETE', 'PUT'])
 def fabricantes():
     if request.method == 'POST':
         nombre = request.form['nombre']
@@ -65,6 +65,36 @@ def fabricantes():
         )
         db.session.add(nuevo_fabricante)
         db.session.commit()
+
+    elif request.method == 'DELETE':
+        fabricante_id = request.json.get('id')
+        if not fabricante_id:
+            return jsonify({"error": "ID is required"}), 400
+
+        fabricante = Fabricante.query.get(fabricante_id)
+        print(fabricante)
+        if fabricante:
+            fabricante.is_active = 0
+            db.session.commit()
+            return jsonify({"message": f"El fabricante se eliminó correctamente"}), 200
+        else:
+            return jsonify({"error": f"fabricante with ID {fabricante_id} not found"}), 404
+
+    elif request.method == 'PUT':
+        fabricante_id = request.json.get('id')
+        nuevo_nombre = request.json.get('nombre')
+        nuevo_pais_origen = request.json.get('paisOrigen')
+        if not fabricante_id or not nuevo_nombre:
+            return jsonify({"error": "ID and new name are required"}), 400
+
+        fabricante = Fabricante.query.get(fabricante_id)
+        if fabricante:
+            fabricante.nombre = nuevo_nombre
+            fabricante.pais_origen = nuevo_pais_origen
+            db.session.commit()
+            return jsonify({"message": f"fabricante with ID {fabricante_id} updated successfully"}), 200
+        else:
+            return jsonify({"error": f"fabricante with ID {fabricante_id} not found"}), 404
 
     fabricantes = Fabricante.query.all()
     return render_template("fabricantes.html", fabricantes=fabricantes)
