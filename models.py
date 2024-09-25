9f0c7e3e8fb998ead1dc919f1c279b435cd4406d
ValenCardozo <valencardozo7@gmail.com>
2024-09-24 21:58:06 -0300

    feat(app): add user model column

--- models.py
+++ models.py
@@ -66,3 +66,4 @@ class User(db.Model):
     username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(200), nullable=False)
     is_active = db.Column(db.Boolean, nullable=False, default=True)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
